d579cb81a02d896262aee7bc1054df89230983c2
Sabrina A <sabrina@Sabrinas-MBP.net>
2022-05-04 16:56:48 -0500

    added functionality to game and updated html/css

--- script.js
+++ script.js
@@ -1,27 +1,86 @@
 const nyanCatPlayer = 'x';
-
-const nyanDogPlayer = 'circle';
-
-const boxElements = document.getElementsByClassName('[data-cell]');
-
-const gameBoard = document.getElementById('game-board');
-
-const winMessageElement = document.getElementById('win-message');
-
-const restartBtn = document.getElementById('restartBtn');
-
-const winMessageTxtElement = document.getElementById('winMessageTxt');
-let = isNyanDogPlayerTurn = false;
-
+const TacoDogPlayer = 'y';
 const winningCombos = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
 ];
+const boxElements = document.querySelectorAll('[data-box]');
+const gameBoard = document.getElementById('gameBoard')
+const winMessage = document.getElementById('winMessage')
+const restartButton = document.getElementById('restartButton')
+const winMessageTxt = document.querySelector('[data-win-message-text]')
+let isTacoDogPlayerTurn 
 
+startPlay();
 
-boxElements.forEach(box => {
-    box.addEventListener('click', handleClick, {once: true} )
-})
+function startPlay(){
+    let isNyanDogPlayerTurn =  false;
+    boxElements.forEach(box => {
+        box.classList.remove(nyanCatPlayer)
+        box.classList.remove(TacoDogPlayer)
+        box.addEventListener('click', handleClick, {once: true})
+    })
+    setBoardHoverPlayer()
+    winMessage.classList.remove('show')
+}
+
 
 function handleClick(e) {
-    console.log('clicked')
+    const box = e.target
+    const currentPlayer = isTacoDogPlayerTurn ? TacoDogPlayer : nyanCatPlayer
+    placeMark(box, currentPlayer)
+    if(checkWin(currentPlayer)) {
+        endGame(false)
+    } else if (isDraw()){
+        endGame(true)
+    } else {
+        switchTurns()
+        setBoardHoverPlayer()
+    }
+    // Check for Win
+    // Check for Draw
+    // Switch Turns
 }
+
+function endGame(draw){
+if (draw) {
+winMessageTxt.innerText = 'Draw!!'
+} else {
+    winMessageTxt.innerText = `${isTacoDogPlayerTurn ? "Taco Dog" : 'Nyan Cat'} Wins !!`
+} 
+    winMessage.classList.add('show')
+}
+
+function isDraw(){
+    return [...boxElements].every(box =>{
+        return box.classList.contains(nyanCatPlayer) || box.classList.contains(TacoDogPlayer)
+    } );{
+    }
+}
+
+function placeMark(box, currentPlayer){
+    box.classList.add(currentPlayer)
+}
+
+function switchTurns(){
+    isTacoDogPlayerTurn = !isTacoDogPlayerTurn
+}
+
+function setBoardHoverPlayer(){
+    gameBoard.classList.remove(nyanCatPlayer)
+    gameBoard.classList.remove(TacoDogPlayer)
+    if(isTacoDogPlayerTurn){
+        gameBoard.classList.add(TacoDogPlayer)
+    } else {
+        gameBoard.classList.add(nyanCatPlayer)
+    }
+}
+
+function checkWin(currentPlayer) {
+return winningCombos.some(combo => {
+    return combo.every(index => {
+        return boxElements[index].classList.contains(currentPlayer)
+    })
+})
+}
+
+restartButton.addEventListener('click', startPlay);
